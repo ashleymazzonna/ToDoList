5f5725e15b7208a75e1f29a2371f04820d33d978
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2020-01-12 19:52:35 -0500

    update data structure

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 //jshint esversion:6
-
 const express = require('./node_modules/express');
 const bodyParser = require("./node_modules/body-parser");
 
 const app = express();
+
 app.set('view engine', 'ejs'); //tells the app to use ejs view engine - must be placed below the app const or it will not exist yet!!1 
+app.use(bodyParser.urlencoded({extended: true }));
+
+var item;
+var items = ["Finish JS Course", "Finish SASS Course"];
 
 app.get("/", function (req, res) {
-
     var today = new Date();
     var currentDay = today.getDay();
     var day = '';
     var name = 'Ashley';
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;    
-        default:
-            console.log("Error: Current day is equal to: " + currentDay)
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long" };
 
-    res.render("lists", { kindOfDay: day, name: name });
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("lists", { 
+        kindOfDay: day,
+        userName: name, 
+        newListItems: items 
+    });
 
 });
+
+app.post("/", function(req, res) {
+    item = req.body.newItem;
+
+    items.push(item);
+
+    // console.log(item);
+
+    res.redirect("/");
+});
+
+
 
 app.listen(3001, function () {
     console.log("server is running on port 3001");
