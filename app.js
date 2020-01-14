701723b19497579cdd59ed456e6112c04dcbc9d5
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2020-01-13 20:11:59 -0500

    Update UI. Add 'work' route for additional lists

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+
 const express = require('./node_modules/express');
 const bodyParser = require("./node_modules/body-parser");
 
@@ -6,40 +7,67 @@ const app = express();
 
 app.set('view engine', 'ejs'); //tells the app to use ejs view engine - must be placed below the app const or it will not exist yet!!1 
 app.use(bodyParser.urlencoded({extended: true }));
+app.use(express.static("public"));
 
 var item;
 var items = ["Finish JS Course", "Finish SASS Course"];
+var workItems = [];
+var today = new Date();
+var currentDay = today.getDay();
+var day = '';
+var name = 'Ashley';
+
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+
+var day = today.toLocaleDateString("en-US", options);
+
+const clearArrs = (arr) => {
+    
+};
+
 
 app.get("/", function (req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = '';
-    var name = 'Ashley';
-
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long" };
-
-    var day = today.toLocaleDateString("en-US", options);
-
     res.render("lists", { 
-        kindOfDay: day,
+        kindOfDay: day, 
+        listsType: 'Home List', 
         userName: name, 
-        newListItems: items 
-    });
+        newListItems: items, 
 
+    });
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     item = req.body.newItem;
 
-    items.push(item);
-
-    // console.log(item);
-
-    res.redirect("/");
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
+
+app.get('/work', function(req, res) {
+    res.render('lists', { 
+        kindOfDay: day, 
+        listsType: "Work List",
+        userName: name,        
+        newListItems: workItems });
+
+});
+
+// app.post('/work', function(req, res) {
+
+//     let item = req.body.newItem;
+//     workItems.push(items);
+//     res.redirect('/work');
+// });
 
 
 
